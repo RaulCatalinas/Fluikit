cd8cc935785e3770310217e7f44ddf04adff330e
Raul Catalinas <raulcatalinas@gmail.com>
2025-11-07 17:56:56 +0100

    chore: File widgets.dart updated

--- lib/widgets.dart
+++ lib/widgets.dart
@@ -3,5 +3,6 @@ export 'src/widgets/drawer.dart';
 export 'src/widgets/dropdown.dart';
 export 'src/widgets/icon_button.dart';
 export 'src/widgets/input.dart';
+export 'src/widgets/slider.dart';
 export 'src/widgets/text.dart';
 export 'src/widgets/text_button.dart';
