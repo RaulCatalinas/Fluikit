d8e684c1939e5ac55cd48551bf3e3c15ac56f346
Raul Catalinas <raulcatalinas@gmail.com>
2025-11-15 19:39:51 +0100

    chore: Unnecessary import removed

--- lib/src/widgets/slider.dart
+++ lib/src/widgets/slider.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart'
         BoxDecoration,
         BuildContext,
         Color,
-        Colors,
         Column,
         Container,
         CrossAxisAlignment,
