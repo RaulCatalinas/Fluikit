3bcf0a184b38e91b3c4ae29b18fcf28140985804
Raul Catalinas <raulcatalinas@gmail.com>
2025-11-15 23:21:18 +0100

    chore: update AppBar Component

--- lib/src/widgets/app_bar.dart
+++ lib/src/widgets/app_bar.dart
@@ -1,22 +1,52 @@
 import 'dart:ui' show Size;
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart'
     show
         AppBar,
         BuildContext,
+        Icons,
         PreferredSizeWidget,
+        Scaffold,
         StatelessWidget,
         Widget,
         kToolbarHeight;
 
+import 'icon_button.dart' show FluiIconButton;
+
 class FluiAppBar extends StatelessWidget implements PreferredSizeWidget {
   final List<Widget> actions;
+  final IconData? drawerIcon;
+  final double? drawerIconSize;
+  final String? drawerIconTooltip;
 
-  const FluiAppBar({super.key, required this.actions});
+  const FluiAppBar({
+    super.key,
+    required this.actions,
+    this.drawerIcon,
+    this.drawerIconSize,
+    this.drawerIconTooltip,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return AppBar(actions: actions);
+    final scaffold = Scaffold.maybeOf(context);
+
+    return AppBar(
+      actions: actions,
+      leading: scaffold != null && scaffold.hasDrawer
+          ? FluiIconButton(
+              icon: drawerIcon ?? Icons.reorder,
+              iconSize: drawerIconSize ?? 24,
+              tooltip: drawerIconTooltip,
+              onPressed: () {
+                scaffold.isDrawerOpen
+                    ? scaffold.closeDrawer()
+                    : scaffold.openDrawer();
+              },
+            )
+          : null,
+    );
   }
 
   @override
